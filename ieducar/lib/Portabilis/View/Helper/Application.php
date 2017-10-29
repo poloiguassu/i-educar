e7328f0bbad5c8bae7e2bf4678731a3e1675ddd7
carmoIn <helcio@carmo.in>
2017-10-29 12:21:59 -0200

    Jquery no header do template base.twig #46
    Dessa forma é possível utilizar tanto o Jquery no PHP quanto nos templates twig.

--- ieducar/lib/Portabilis/View/Helper/Application.php
+++ ieducar/lib/Portabilis/View/Helper/Application.php
@@ -47,151 +47,151 @@ require_once 'Portabilis/Assets/Version.php';
 
 class Portabilis_View_Helper_Application extends CoreExt_View_Helper_Abstract {
 
-  // previne carregar mais de uma vez o mesmo asset js ou css
-  protected static $javascriptsLoaded = array();
-  protected static $stylesheetsLoaded = array();
+	// previne carregar mais de uma vez o mesmo asset js ou css
+	protected static $javascriptsLoaded = array();
+	protected static $stylesheetsLoaded = array();
 
 
-  /**
-   * Construtor singleton.
-   */
-  protected function __construct()
-  {
-  }
+	/**
+	* Construtor singleton.
+	*/
+	protected function __construct()
+	{
+	}
 
 
-  /**
-   * Retorna uma instância singleton.
-   * @return CoreExt_View_Helper_Abstract
-   */
-  public static function getInstance()
-  {
-    return self::_getInstance(__CLASS__);
-  }
+	/**
+	* Retorna uma instância singleton.
+	* @return CoreExt_View_Helper_Abstract
+	*/
+	public static function getInstance()
+	{
+		return self::_getInstance(__CLASS__);
+	}
 
 
 
-  /**
-   * Adiciona elementos chamadas scripts javascript para instancia da view recebida, exemplo:
-   *
-   * <code>
-   * $applicationHelper->javascript($viewInstance, array('/modules/ModuleName/Assets/Javascripts/ScriptName.js', '...'));
-   * </code>
-   *
-   * @param   object   $viewInstance  Istancia da view a ser carregado os scripts.
-   * @param   array ou string  $files  Lista de scripts a serem carregados.
-   * @return  null
-   */
-  public static function loadJavascript($viewInstance, $files, $appendAssetsVersionParam = true) {
-    if (! is_array($files))
-      $files = array($files);
-
-    foreach ($files as $file) {
-      // somente carrega o asset uma vez
-      if (! in_array($file, self::$javascriptsLoaded)) {
-        self::$javascriptsLoaded[] = $file;
-
-        if ($appendAssetsVersionParam)
-          $file .= '?assets_version=' . Portabilis_Assets_Version::VERSION;
-
-        $viewInstance->appendOutput("<script type='text/javascript' src='$file'></script>");
-      }
-    }
-  }
+	/**
+	* Adiciona elementos chamadas scripts javascript para instancia da view recebida, exemplo:
+	*
+	* <code>
+	* $applicationHelper->javascript($viewInstance, array('/modules/ModuleName/Assets/Javascripts/ScriptName.js', '...'));
+	* </code>
+	*
+	* @param   object   $viewInstance  Istancia da view a ser carregado os scripts.
+	* @param   array ou string  $files  Lista de scripts a serem carregados.
+	* @return  null
+	*/
+	public static function loadJavascript($viewInstance, $files, $appendAssetsVersionParam = true)
+	{
+		if (! is_array($files))
+		$files = array($files);
+		foreach ($files as $file) {
+			// somente carrega o asset uma vez
+			if (! in_array($file, self::$javascriptsLoaded)) {
+				self::$javascriptsLoaded[] = $file;
+				if ($appendAssetsVersionParam)
+				$file .= '?assets_version=' . Portabilis_Assets_Version::VERSION;
+				$viewInstance->appendOutput("<script type='text/javascript' src='$file'></script>");
+			}
+		}
+	}
 
 
-  /**
-   * Adiciona links css para instancia da view recebida, exemplo:
-   *
-   * <code>
-   * $applicationHelper->stylesheet($viewInstance, array('/modules/ModuleName/Assets/Stylesheets/StyleName.css', '...'));
-   * </code>
-   *
-   * @param   object   $viewInstance1  Istancia da view a ser adicionado os links para os estilos.
-   * @param   array ou string  $files  Lista de estilos a serem carregados.
-   * @return  null
-   */
-  public static function loadStylesheet($viewInstance, $files, $appendAssetsVersionParam = true) {
-    if (! is_array($files))
-      $files = array($files);
-
-    foreach ($files as $file) {
-      // somente carrega o asset uma vez
-      if (! in_array($file, self::$stylesheetsLoaded)) {
-        self::$stylesheetsLoaded[] = $file;
-
-        if ($appendAssetsVersionParam)
-          $file .= '?assets_version=' . Portabilis_Assets_Version::VERSION;
-
-        $viewInstance->appendOutput("<link type='text/css' rel='stylesheet' href='$file'></script>");
-      }
-    }
-  }
+	/**
+	* Adiciona links css para instancia da view recebida, exemplo:
+	*
+	* <code>
+	* $applicationHelper->stylesheet($viewInstance, array('/modules/ModuleName/Assets/Stylesheets/StyleName.css', '...'));
+	* </code>
+	*
+	* @param   object   $viewInstance1  Istancia da view a ser adicionado os links para os estilos.
+	* @param   array ou string  $files  Lista de estilos a serem carregados.
+	* @return  null
+	*/
+	public static function loadStylesheet($viewInstance, $files, $appendAssetsVersionParam = true)
+	{
+		if (! is_array($files))
+		$files = array($files);
+		foreach ($files as $file) {
+			// somente carrega o asset uma vez
+			if (! in_array($file, self::$stylesheetsLoaded)) {
+				self::$stylesheetsLoaded[] = $file;
+				if ($appendAssetsVersionParam)
+				$file .= '?assets_version=' . Portabilis_Assets_Version::VERSION;
+				$viewInstance->appendOutput("<link type='text/css' rel='stylesheet' href='$file'></script>");
+			}
+		}
+	}
 
 
-  public static function embedJavascript($viewInstance, $script, $afterReady = false) {
-    if ($afterReady) {
-      self::loadJQueryLib($viewInstance);
-
-      $script = "(function($){
-        $(document).ready(function(){
-          $script
-        });
-      })(jQuery);";
-    }
-
-    $viewInstance->appendOutput("<script type='text/javascript'>$script</script>");
-  }
-
-
-  public static function embedStylesheet($viewInstance, $css) {
-    $viewInstance->appendOutput("<style type='text/css'>$css</style>");
-  }
-
-  public static function embedJavascriptToFixupFieldsWidth($viewInstance) {
-    Portabilis_View_Helper_Application::loadJQueryLib($viewInstance);
-
-    Portabilis_View_Helper_Application::loadJavascript(
-      $viewInstance, '/modules/Portabilis/Assets/Javascripts/Utils.js'
-    );
-
-    Portabilis_View_Helper_Application::embedJavascript(
-      $viewInstance, 'fixupFieldsWidth();', $afterReady = true
-    );
-
-  }
-
-  // load lib helpers
-
-  public static function loadJQueryLib($viewInstance) {
-    self::loadJavascript($viewInstance, '//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js', false);
-    self::embedJavascript($viewInstance, "if (typeof(\$j) == 'undefined') { var \$j = jQuery.noConflict(); }");
-  }
+	public static function embedJavascript($viewInstance, $script, $afterReady = false)
+	{
+		if ($afterReady) {
+			self::loadJQueryLib($viewInstance);
+			$script = "(function($){
+				$(document).ready(function(){
+					$script
+				});
+			})(jQuery);";
+		}
+		$viewInstance->appendOutput("<script type='text/javascript'>$script</script>");
+	}
 
 
-  public static function loadJQueryFormLib($viewInstance) {
-    self::loadJavascript($viewInstance, 'scripts/jquery/jquery.form.js', false);
-  }
+	public static function embedStylesheet($viewInstance, $css) {
+		$viewInstance->appendOutput("<style type='text/css'>$css</style>");
+	}
 
+	public static function embedJavascriptToFixupFieldsWidth($viewInstance)
+	{
+		Portabilis_View_Helper_Application::loadJQueryLib($viewInstance);
+		Portabilis_View_Helper_Application::loadJavascript(
+			$viewInstance, '/modules/Portabilis/Assets/Javascripts/Utils.js'
+		);
+		Portabilis_View_Helper_Application::embedJavascript(
+			$viewInstance, 'fixupFieldsWidth();', $afterReady = true
+		);
+	}
 
-  public static function loadJQueryUiLib($viewInstance) {
-    self::loadJavascript($viewInstance, '//ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js', false);
-    self::loadStylesheet($viewInstance, '//ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/themes/ui-lightness/jquery-ui.css', false);
+	// load lib helpers
 
-    // ui-autocomplete fixup
-    self::embedStylesheet($viewInstance, ".ui-autocomplete { font-size: 11px; }");
-  }
+	public static function loadJQueryLib($viewInstance)
+	{
+		//self::loadJavascript($viewInstance, 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js', false);
+		//self::embedJavascript($viewInstance, "if (typeof(\$j) == 'undefined') { var \$j = jQuery.noConflict(); }");
+	}
 
-  public static function loadChosenLib($viewInstance) {
-    self::loadStylesheet($viewInstance, '/modules/Portabilis/Assets/Plugins/Chosen/chosen.css', false);
-    self::loadJavascript($viewInstance, '/modules/Portabilis/Assets/Plugins/Chosen/chosen.jquery.min.js', false);
-  }
+	public static function loadMomentJsLib($viewInstance)
+	{
+		self::loadJavascript($viewInstance, '/assets/js/moment.js', false);
+		self::loadJavascript($viewInstance, '/assets/js/moment-weekday-calc.js', false);
+	}
 
-  public static function loadAjaxChosenLib($viewInstance) {
-    // AjaxChosen requires this fixup, see https://github.com/meltingice/ajax-chosen
-    $fixupCss = ".chzn-container .chzn-results .group-result { display: list-item; }";
-    Portabilis_View_Helper_Application::embedStylesheet($viewInstance, $fixupCss);
+	public static function loadJQueryFormLib($viewInstance)
+	{
+		self::loadJavascript($viewInstance, 'scripts/jquery/jquery.form.js', false);
+	}
 
-    self::loadJavascript($viewInstance, '/modules/Portabilis/Assets/Plugins/AjaxChosen/ajax-chosen.min.js', false);
-  }
+	public static function loadJQueryUiLib($viewInstance)
+	{
+		self::loadJavascript($viewInstance, '//ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js', false);
+		self::loadStylesheet($viewInstance, '//ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/themes/ui-lightness/jquery-ui.css', false);
+		// ui-autocomplete fixup
+		self::embedStylesheet($viewInstance, ".ui-autocomplete { font-size: 11px; }");
+	}
+
+	public static function loadChosenLib($viewInstance)
+	{
+		self::loadStylesheet($viewInstance, '/modules/Portabilis/Assets/Plugins/Chosen/chosen.css', false);
+		self::loadJavascript($viewInstance, '/modules/Portabilis/Assets/Plugins/Chosen/chosen.jquery.min.js', false);
+	}
+
+	public static function loadAjaxChosenLib($viewInstance)
+	{
+		// AjaxChosen requires this fixup, see https://github.com/meltingice/ajax-chosen
+		$fixupCss = ".chzn-container .chzn-results .group-result { display: list-item; }";
+		Portabilis_View_Helper_Application::embedStylesheet($viewInstance, $fixupCss);
+		self::loadJavascript($viewInstance, '/modules/Portabilis/Assets/Plugins/AjaxChosen/ajax-chosen.min.js', false);
+	}
 }
