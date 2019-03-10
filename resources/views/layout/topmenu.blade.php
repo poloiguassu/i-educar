26c50ff772f3d970967d78715494f87023866ad8
Helcio <helcio@carmo.in>
2019-03-09 23:17:37 -0300

    Corrigindo identação

--- resources/views/layout/topmenu.blade.php
+++ resources/views/layout/topmenu.blade.php
@@ -5,9 +5,9 @@
     @foreach ($topmenu->getTopMenuArray(session('id_pessoa')) as $item)
         array_menu[array_menu.length] = ['{{$item['tt_menu']}} ', {{$item['cod_menu']}},'{{$item['ref_cod_menu_pai']}}','', 'null', '{{$item['caminho']}}', '{{$item['alvo']}}'];
 
-    @if(!$item['ref_cod_menu_pai'])
-        array_id[array_id.length] = {{$item['cod_menu']}};
-    @endif
+        @if(!$item['ref_cod_menu_pai'])
+            array_id[array_id.length] = {{$item['cod_menu']}};
+        @endif
     @endforeach
 </script>
 <script type="text/javascript">
