71081f30640dc339976c78e78805c200a7751d26
Helcio <helcio@carmo.in>
2019-03-09 22:14:21 -0300

    Adicionando campo área de interesse a query de inscritos

--- ieducar/intranet/include/pmieducar/clsPmieducarInscrito.inc.php
+++ ieducar/intranet/include/pmieducar/clsPmieducarInscrito.inc.php
@@ -89,8 +89,8 @@ class clsPmieducarInscrito
 
         $this->_campos_lista = $this->_todos_campos = 'i.cod_inscrito,
             i.ref_cod_selecao_processo, i.ref_cod_aluno, i.estudando_serie, i.egresso,
-            i.estudando_turno, i.guarda_mirim, i.copia_rg, i.copia_residencia,
-            i.copia_historico, i.copia_renda, i.encaminhamento, i.ref_usuario_exc,
+            i.estudando_turno, i.guarda_mirim, i.copia_rg, i.copia_cpf, i.copia_residencia,
+            i.copia_historico, i.copia_renda, i.area_interesse, i.encaminhamento, i.ref_usuario_exc,
             i.ref_usuario_cad, i.data_cadastro, i.data_exclusao, i.ativo';
     }
 
